3aafd9c6c60454c6a69c9e14ef72503a4c7edee0
Julien <julien.genty.contact@gmail.com>
2026-01-21 22:06:57 +0100

    feature (component): added of Player and Scene component

---
+++ src/Game.tsx
@@ -0,0 +1,10 @@
+import { Scene } from './components/Scene.tsx';
+import { Player } from './components/Player.tsx';
+
+export default function Game(){
+    return (
+        <Scene>
+            <Player />
+        </Scene>
+    )
+}

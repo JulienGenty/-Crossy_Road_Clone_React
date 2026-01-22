f7a96472294ffa4dcbb6982439d3deeb003d7373
Julien <julien.genty.contact@gmail.com>
2026-01-22 14:35:05 +0100

    feature (component): add of Grass component

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,10 +1,12 @@
-import { Scene } from './components/Scene.tsx';
-import { Player } from './components/Player.tsx';
+import { Scene } from "./components/Scene.tsx";
+import { Player } from "./components/Player.tsx";
+import { Map } from "./components/Map.tsx";
 
-export default function Game(){
-    return (
-        <Scene>
-            <Player />
-        </Scene>
-    )
+export default function Game() {
+  return (
+    <Scene>
+      <Player />
+      <Map />
+    </Scene>
+  );
 }
---
+++ src/components/Map.tsx
@@ -0,0 +1,9 @@
+import { Grass } from "./Grass.tsx";
+
+export function Map() {
+  return (
+    <>
+      <Grass rowIndex={0} />
+    </>
+  );
+}

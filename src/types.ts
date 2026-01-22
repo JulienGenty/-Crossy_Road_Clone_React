29810e00a1b20889fb3a228cdeeeb379bb3f6c67
Julien <julien.genty.contact@gmail.com>
2026-01-22 14:16:14 +0100

    feature (types): added of types and constants

---
+++ src/types.ts
@@ -0,0 +1,19 @@
+import * as THREE from "three";
+
+export type RowType = "forest" | "car" | "truck";
+
+export type Row =
+    | {
+        type: "forest",
+        tress: { tileIndex: number; height: number; }[];
+    } | {
+        type: "car",
+        direction: boolean,
+        speed: number,
+        vehicles: { initialTileIndex: number; color: THREE.ColorRepresentation; }[];
+    } | {
+        type: "truck",
+        direction: boolean,
+        speed: number,
+        vehicles: { initialTileIndex: number; color: THREE.ColorRepresentation; }[];
+    };
